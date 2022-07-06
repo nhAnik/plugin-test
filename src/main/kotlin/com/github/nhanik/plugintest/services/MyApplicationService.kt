62e8c02c7b738964cba1be1e6f030aa6e82e000e
GitHub Action <action@github.com>
2022-07-06 14:02:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/nhanik/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nhanik/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nhanik.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nhanik.plugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nhanik/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nhanik/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nhanik.plugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nhanik.plugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
